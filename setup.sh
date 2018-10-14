d22ebbfa4efb1f87b18a4de0d983526e7fe8e303
binliu-aipnd <43527958+binliu-aipnd@users.noreply.github.com>
2018-10-14 19:41:34 +0800

    update

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ export PATH=/home/nbuser/anaconda3_420/bin:$PATH
 
 cd /home/nbuser/library
 pip install -r requirements.txt
-conda install -c conda-forge wordcloud
+echo 'y\ny\n' | conda install -c conda-forge wordcloud
